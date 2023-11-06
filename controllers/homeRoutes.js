6d7d319c2f02639e98ec10a546932d28529ca44b
jwhit <28734055+jwhit-commit@users.noreply.github.com>
2023-11-06 16:02:21 -0600

    seeded test data, setup home route w/ posts displaying dynamically

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,15 +1,22 @@
 const router = require('express').Router();
-// const { User, Location, Station } = require('../models');
+const { User, Post } = require('../models');
 // const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
     try {
-      res.render('home', { 
-        logged_in: req.session.logged_in 
-      });
+        const dbPostData = await Post.findAll();
+
+        const posts = dbPostData.map((post) =>
+        post.get({ plain: true })
+      );
+
+        res.render('home', { 
+            posts,
+            logged_in: req.session.logged_in 
+        });
     } catch (err) {
-      console.error(err);
-      res.status(500).json(err);
+        console.error(err);
+        res.status(500).json(err);
     }
   });
 
